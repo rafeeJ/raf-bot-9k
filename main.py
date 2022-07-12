34ac34d3c5b0959f1ab636340b4b77f9aedb55c5
Rafee Jenkins <rafeejenkins@gmail.com>
2022-07-12 21:19:29 +0100

    added failure msg

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ bot = commands.Bot(command_prefix='$')
 @bot.command()
 async def roll(ctx, dice_size):
     result = re.match('d(\d{1,2})', dice_size, flags=re.IGNORECASE)
-    n = randrange(int(result.group(1)))
-    await ctx.send('You rolled a {}'.format(n + 1))
+    if len(result) > 0:
+        n = randrange(int(result.group(1)))
+        await ctx.send('You rolled a {}'.format(n + 1))
+    else:
+        await ctx.send('Syntax is: $roll d<number>')
 
 bot.run(os.environ['token'])
