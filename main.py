7d61a8452b8a438d79394b2f0cc54eaa22265dc2
Rafee Jenkins <rafeejenkins@gmail.com>
2022-07-12 21:43:34 +0100

    Added some more pizzaz

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ bot = commands.Bot(command_prefix='$')
 async def roll(ctx, dice_size):
     result = re.match('d(\d{1,2})', dice_size, flags=re.IGNORECASE)
     if result:
-        n = randrange(int(result.group(1)))
-        await ctx.send('You rolled a {}'.format(n + 1))
+        dice_to_roll = int(result.group(1))
+        rolled_number = randrange(dice_to_roll) + 1
+        if rolled_number == dice_to_roll:
+            await ctx.send('Oh baby, a natural {}'.format(rolled_number))
+        else:
+            await ctx.send('You rolled a {}'.format(rolled_number), file=discord.file('imgs/nat.gif'))
     else:
-        await ctx.send('Syntax is: $roll d<number>')
+        await ctx.send('Syntax is: $roll d<number>', delete_after=5)
 
 bot.run(os.environ['token'])
