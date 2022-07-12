8bd91898c9df3f046cfded5d2f042d660c9454d0
Rafee Jenkins <rafeejenkins@gmail.com>
2022-07-12 21:15:52 +0100

    Fixed int bug lol

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import discord
 from discord.ext import commands
+
 import os
+import re
 from random import randrange
 
 bot = commands.Bot(command_prefix='$')
 
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
-
-@bot.command()
 async def roll(ctx, dice_size):
-    n = randrange(dice_size)
+    result = re.match('d(\d{1,2})', dice_size, flags=re.IGNORECASE)
+    n = randrange(int(result.group(1)))
     await ctx.send('You rolled a {}'.format(n + 1))
 
 bot.run(os.environ['token'])
