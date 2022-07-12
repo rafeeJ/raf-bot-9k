40e17fef1f573ca5abf022bfe117c37e4022f164
Rafee Jenkins <rafeejenkins@gmail.com>
2022-07-12 22:22:40 +0100

    Added some more validation

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ async def roll(ctx, dice_size):
     result = re.match('d(\d{1,8})', dice_size, flags=re.IGNORECASE)
     if result:
         dice_to_roll = int(result.group(1))
+        if dice_to_roll == 0:
+            await ctx.send('Stop trying to roll a 0', delete_after=5)
+            return False
         rolled_number = randrange(dice_to_roll) + 1
-        if rolled_number == dice_to_roll:
+        if rolled_number == dice_to_roll and dice_to_roll != 1:
             await ctx.send('Oh baby, a natural {}!'.format(rolled_number), file=discord.File('imgs/nat.gif'))
         elif rolled_number == 1 and dice_to_roll != 1:
             await ctx.send('Hhahaha natural {} hahahah'.format(rolled_number), file=discord.File('imgs/fail.gif'))
